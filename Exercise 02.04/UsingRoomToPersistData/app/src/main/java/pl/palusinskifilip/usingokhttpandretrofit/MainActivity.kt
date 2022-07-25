9d444d4bd4b9320a9c42c407c8f462a5c12b5c52
Filip Palusiński <palusinskifilip@gmail.com>
2022-07-25 19:12:01 +0200

    Completed exercise 02.04

---
+++ Exercise 02.04/UsingRoomToPersistData/app/src/main/java/pl/palusinskifilip/usingokhttpandretrofit/MainActivity.kt
@@ -0,0 +1,56 @@
+package pl.palusinskifilip.usingokhttpandretrofit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.Screen
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import pl.palusinskifilip.usingokhttpandretrofit.ui.theme.UsingOkHttpAndRetrofitTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            UsingOkHttpAndRetrofitTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Screen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Screen(viewModel: MainViewModel = viewModel
+    (factory = MainViewModelFactory())) {
+    UserList(users = viewModel.resultState)
+}
+
+@Composable
+fun UserList(users: List<UserEntity>) {
+    LazyColumn(modifier = Modifier.padding(16.dp)) {
+        items(users) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(text = it.name)
+                Text(text = it.username)
+                Text(text = it.email)
+            }
+        }
+    }
+}
---
+++ Exercise 02.04/UsingRoomToPersistData/app/src/main/java/pl/palusinskifilip/usingokhttpandretrofit/UserDao.kt
@@ -0,0 +1,16 @@
+package pl.palusinskifilip.usingokhttpandretrofit
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user")
+    fun getUsers(): Flow<List<UserEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertUsers(users: List<UserEntity>)
+}
